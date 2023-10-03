7f3332aed03798d121e88ebf7c84bca4146474a4
abdalarhmanmhnaya <abdalrhmanmonaya@gmail.com>
2023-10-03 13:30:55 -0700

    footer comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 
@@ -9,9 +10,7 @@ function App() {
   <main>
     content
   </main>
-  <footer>
-    footer
-  </footer>
+<Footer/>
 
 
      </div>
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+function Footer() {
+  return (
+    <footer>
+        &copy; CopyWright Abdalrhman Mohnaya 2023
+    </footer>
+  )
+}
+export default Footer
