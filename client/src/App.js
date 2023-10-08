a8fe7c17b03b805e36af6989c95a8eaa9e28e239
abdalarhmanmhnaya <abdalrhmanmonaya@gmail.com>
2023-10-08 05:36:31 -0700

    products comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
+import React , {useState} from 'react'
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import { wordes } from "./words";
-
+//import { wordes } from "./words";
+import data from './dta.json'
+import Products from './components/Products/Products';
 
 function App() {
+
+  const [products, setProducts] = useState (data)  
+
+
   return (
   
      <div className="layout"> 
 <Header/>
   <main>
-    {wordes.content}
+  <div className="wrapper">
+     <Products products={products}/>
+    <div className="filter-wrapper">Filter</div>
+  </div>
   </main>
 <Footer/>
 
